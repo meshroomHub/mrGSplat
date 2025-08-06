ad69f4085bc30f0abc136aeaec9bc602319693cd
Alice Sonolet <alice.sonolet@gmail.com>
2025-08-06 17:51:19 +0200

    Render: Add format and colorspace outputs

--- nodes/optim/GaussianSplattingRender.py
+++ nodes/optim/GaussianSplattingRender.py
@@ -3,11 +3,18 @@ __version__ = "1.0"
 from meshroom.core import desc
 import os.path
 
-currentDir = os.path.dirname(os.path.abspath(__file__))
+COLOR_SPACES = [
+    "AUTO", "LINEAR", "SRGB", "ACES2065_1", "ACEScg", "REC709", "Linear_ARRI_Wide_Gamut_3", 
+    "ARRI_LogC3_EI800", "Linear_ARRI_Wide_Gamut_4", "ARRI_LogC4", "Linear_BMD_WideGamut_Gen5", 
+    "BMDFilm_WideGamut_Gen5", "CanonLog2_CinemaGamut_D55", "CanonLog3_CinemaGamut_D55", 
+    "Linear_CinemaGamut_D55", "Linear_V_Gamut", "V_Log_V_Gamut", "Linear_REDWideGamutRGB", 
+    "Log3G10_REDWideGamutRGB", "Linear_Venice_S_Gamut3_Cine", "S_Log3_Venice_S_Gamut3_Cine", 
+    "LAB", "XYZ", "NO_CONVERSION"
+]
 
 class GaussianSplattingRender(desc.CommandLineNode):
 
-    commandLine = 'rez env {rezEnvNameValue} -- gaussianSplattingRender --model {modelValue} --cameras {camerasValue} --data_factor {resolutionFactorValue} --output {cache}/{nodeType}/{uid}'
+    commandLine = 'rez env {rezEnvNameValue} -- gaussianSplattingRender --model {modelValue} --cameras {camerasValue} --data_factor {resolutionFactorValue} --output {cache}/{nodeType}/{uid} --outputFormat {outputFormatValue}'
     
     gpu = desc.Level.INTENSIVE
     cpu = desc.Level.NORMAL
@@ -54,6 +61,22 @@ class GaussianSplattingRender(desc.CommandLineNode):
             value=1,
             group="",
             exposed=False,
+        ),
+        desc.ChoiceParam(
+            name='outputFormat',
+            label='Output format',
+            description='''auto will try to find the extension from views in the sfm (cameras) input''',
+            value='auto',
+            values=['auto', '.jpg', '.png', '.exr'],
+            exclusive=True,
+        ),
+        desc.ChoiceParam(
+            name='outputColorspace',
+            label='Output colorspace',
+            description='''''',
+            value='AUTO',
+            values=COLOR_SPACES,
+            exclusive=True,
         ),
     ]
 
